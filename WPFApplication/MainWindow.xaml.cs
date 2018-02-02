9869a2b4d1bbbaf10582b3084c9120573a0652c7
Akram Eldamaty <33161840+AkramEld@users.noreply.github.com>
2018-02-02 01:23:50 -0500

    Added Main files

---
+++ WPFApplication/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace WPFApplication
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public String address;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SubmitButton_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void Confirm_Click(object sender, RoutedEventArgs e)
+        {
+            address = postalcode.Text;
+            map win2 = new map();
+            win2.Show();
+        }
+    }
+}
